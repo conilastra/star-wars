5103b4d0eb2345fb4dc46e0b1b30c0c48bd44c65
conilastra <cflastra@uc.cl>
2019-10-31 18:50:55 -0300

    actualización del componente Card

--- src/js/component/cardHolder.js
+++ src/js/component/cardHolder.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { Card } from "./card";
 import PropTypes from "prop-types";
+import { Consumer } from "../store/appContext";
 
 export class CardHolder extends React.Component {
 	render() {
+		const { title } = this.props;
+
 		return (
 			<section className="container py-2 mb-5 vw-100">
-				<h2 className="text-danger py-3">{this.props.title}</h2>
+				<h2 className="text-danger py-3">{title}</h2>
 				<div className="row flex-row flex-nowrap overflow-auto">
-					<Card type="characters" />
+					<Consumer>
+						{({ store }) => {
+							return store["characters"].map(item => (
+								<Card
+									name={item.name}
+									gender={item.gender}
+									hair={item.hair}
+									eyes={item.eyes}
+									key={item.name}
+								/>
+							));
+						}}
+					</Consumer>
 				</div>
 			</section>
 		);
@@ -16,5 +31,9 @@ export class CardHolder extends React.Component {
 }
 
 CardHolder.propTypes = {
-	title: PropTypes.string
+	title: PropTypes.string,
+	name: PropTypes.string,
+	gender: PropTypes.string,
+	eyes: PropTypes.string,
+	hair: PropTypes.string
 };
